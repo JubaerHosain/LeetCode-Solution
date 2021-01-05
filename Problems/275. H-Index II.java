11ceba46eda55b6224aa4247a9200b5b3e8792ad
JubaerHosain <61691077+JubaerHosain@users.noreply.github.com>
2021-01-05 21:30:17 +0600

    Create 275. H-Index II.java

---
+++ Problems/275. H-Index II.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int hIndex(int[] nums) {
+        int n = nums.length;
+        int left = 0, right = n-1;
+        
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            if(nums[mid] == n-mid){
+                return nums[mid];
+            } else if(nums[mid] > n-mid){
+                right = mid-1;
+            } else {
+                left = mid+1;
+            }
+        }
+        
+        return n-left;
+    }
+}
