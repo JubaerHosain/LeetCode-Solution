c1c30257317d645078f78f3125e690a616a5fc76
JubaerHosain <61691077+JubaerHosain@users.noreply.github.com>
2021-01-06 14:49:03 +0600

    Create 922. Sort Array By Parity II.java

---
+++ Problems/922. Sort Array By Parity II.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] sortArrayByParityII(int[] A) {
+        int start = 0, end = A.length-1;
+        
+        while(start < A.length-1){
+            if(A[start]%2 == 0){
+                start += 2;
+            } else if(A[end]%2 != 0){
+                end -= 2;
+            } else {
+                int temp = A[start];
+                A[start] = A[end];
+                A[end] = temp;
+            }
+        }
+        
+        return A;
+    }
+}
